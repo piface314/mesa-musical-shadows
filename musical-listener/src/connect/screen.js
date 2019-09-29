f53212f71bb432367d55c3ba3896f9d688b04078
Henrique Santana <henriquessantana314@gmail.com>
2019-09-29 00:41:55 -0300

    Iniciando a tela inicial

--- musical-listener/src/connect/screen.js
+++ musical-listener/src/connect/screen.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { Text, StatusBar, SafeAreaView, Image } from 'react-native';
+import { styles } from '../theme';
 
 export default class ConnectScreen extends Component {
-  static navigationOptions = ({ navigation }) => {
-    const device = navigation.getParam('device');
-    return {
-      title: device.name || device.id,
-    };
+  static navigationOptions = {
+    title: 'Mesa Musical Shadows',
+    headerStyle: styles.header,
+    headerTitleStyle: styles.headerTitle,
+    headerTintColor: "white"
   };
-  render() {
 
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle="light-content" backgroundColor={styles.header.backgroundColor} />
+        <Image source={require("../../assets/logo.png")} style={{height:200, width:200}} />
+      </SafeAreaView>
+    );
   }
 }
--- musical-listener/src/theme.js
+++ musical-listener/src/theme.js
@@ -1,15 +1,22 @@
 import { StyleSheet } from 'react-native';
 
 export const colors = {
-  roselight: '#ff769c',
-  rosepulse: '#ff5d91',
-  purplelink: '#c393ff',
-  bluesoft: '#006aff',
-  bluedeep: '#1b45ff'
+  roseLight: '#ff769c',
+  rosePulse: '#ff5d91',
+  purpleLink: '#c393ff',
+  blueSoft: '#006aff',
+  blueDeep: '#1b45ff',
+  blueDeeper: '#1a39ba'
 };
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-  }
+  },
+  header: {
+    backgroundColor: colors.blueSoft
+  },
+  headerTitle: {
+    
+  },
 });
